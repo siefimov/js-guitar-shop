2f5f9a88f0d5e1f51e700b45968f6a98f7f04e02
serhii yefimov <sergii.iefimov@gmail.com>
2022-05-06 18:48:50 +0300

    catalog ended

--- constants/catalog.js
+++ constants/catalog.js
@@ -1,20 +1,72 @@
 const CATALOG = [
   {
     id: 'el1',
-    name: '',
-    img: '',
-    price: 0
+    name: 'Свічка пальмова ЧЕРВОНА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-19.jpg?fit=1638%2C2396&ssl=1',
+    price: 150
   },
   {
     id: 'el2',
-    name: '',
-    img: '',
-    price: 0
+    name: 'Свічка пальмова МОККО',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-16.jpg?resize=700%2C1024&ssl=1',
+    price: 50
   },
   {
     id: 'el3',
-    name: '',
-    img: '',
-    price: 0
+    name: 'Свічка пальмова РОЖЕВИЙ ПЕРЛАМУТР',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-15.jpg?resize=700%2C1024&ssl=1',
+    price: 150
+  },
+  {
+    id: 'el4',
+    name: 'Свічка пальмова ЧАЙНАЯ ТРОЯНДА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2021/03/67-3-03.jpg?resize=700%2C1024&ssl=1',
+    price: 150
+  },
+  {
+    id: 'el5',
+    name: 'Свічка пальмова РУБІНОВА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-18.jpg?resize=700%2C1024&ssl=1',
+    price: 150
+  },
+  {
+    id: 'el6',
+    name: 'Свічка пальмова СМАРАГДОВА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-74.jpg?resize=700%2C1024&ssl=1',
+    price: 150
+  },
+  {
+    id: 'el7',
+    name: 'Свічка пальмова ЧОРНА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2019/10/67-3-26.jpg?resize=700%2C1024&ssl=1',
+    price: 150
+  },
+  {
+    id: 'el8',
+    name: 'Свічка столова, РУБІНОВА',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2022/02/18-scaled.jpg?resize=768%2C1024&ssl=1',
+    price: 50
+  },
+  {
+    id: 'el9',
+    name: 'Свічка ароматична ЧОРНЕ МОРЕ',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2021/10/08-1.png?resize=768%2C969&ssl=1',
+    price: 320
+  },
+  {
+    id: 'el10',
+    name: 'Віск для насипної свічки, ГАРБУЗОВИЙ, 200 г',
+    img:
+      'https://i0.wp.com/plamis.com.ua/wp-content/uploads/2021/03/PW-200-52-scaled.jpg?resize=691%2C1024&ssl=1',
+    price: 150
   }
 ]
