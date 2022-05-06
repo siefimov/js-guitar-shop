c842fa1eb0e2faddcf8b62c530fa6ba952944402
serhii yefimov <sergii.iefimov@gmail.com>
2022-05-06 18:10:17 +0300

    catalog

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+  {
+    id: 'el1',
+    name: '',
+    img: '',
+    price: 0
+  },
+  {
+    id: 'el2',
+    name: '',
+    img: '',
+    price: 0
+  },
+  {
+    id: 'el3',
+    name: '',
+    img: '',
+    price: 0
+  }
+]
