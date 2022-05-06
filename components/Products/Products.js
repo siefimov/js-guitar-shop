95c1422d326aa4b9879a0546477bb4e7aa5702f2
serhii yefimov <sergii.iefimov@gmail.com>
2022-05-06 19:50:03 +0300

    layout component catalog products

---
+++ components/Products/Products.js
@@ -0,0 +1,35 @@
+'use strict'
+
+class Products {
+  render () {
+    let htmlCatalog = ''
+
+    //   виводить об'єкти
+    // CATALOG.forEach((elem) => {
+    //    console.log(elem)
+    //  })
+
+    // застосовуємо деструктуризацію -
+    CATALOG.forEach(({ id, name, price, img }) => {
+      htmlCatalog += `
+      <li>
+         <span>${name}</span>
+         <img src="${img}" />
+         <span>${price}</span>
+         <button>Додати до кошика</button>
+      </li>
+      `
+    })
+
+    const html = `
+      <ul>
+         ${htmlCatalog}
+      </ul>
+      `
+
+      ROOT_PRODUCTS.innerHTML = html
+  }
+}
+
+const productPage = new Products()
+productPage.render()
